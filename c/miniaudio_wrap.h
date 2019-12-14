eaa789ea3803f2d02b83a9c35129ab8a74e0cd1b
lmp <dev.larpon@gmail.com>
2019-12-14 12:24:51 +0100

    Add C implementation

---
+++ c/miniaudio_wrap.h
@@ -0,0 +1,12 @@
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+    if (pDecoder == NULL) {
+        return;
+    }
+
+    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount);
+
+    (void)pInput;
+}
